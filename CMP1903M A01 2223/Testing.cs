18dc80c243f8522763597de27170912df19a5e94
Marcus Sanghvi <26944375@students.lincoln.ac.uk>
2023-03-08 16:50:15 +0000

    final

--- CMP1903M A01 2223/Testing.cs
+++ CMP1903M A01 2223/Testing.cs
@@ -31,18 +31,16 @@ namespace CMP1903M_A01_2223
                     Card dealedCard = Pack.deal();
 
                     Console.WriteLine(dealedCard.Value + " of " + dealedCard.Suit); // Returns the card in a format the user can understand
-                    Console.ReadLine();
+                    
                 }
                 else
                 {
                     Console.WriteLine("No Shuffle.. Dealing a Card");
-                    Console.ReadLine();
 
                     Pack.shuffleCardPack(3);
                     Card dealedCard = Pack.deal();
 
                     Console.WriteLine(dealedCard.Value + " of " + dealedCard.Suit); // Returns the card in a format the user can understand
-                    Console.ReadLine();
                 }
             }
             else
@@ -56,7 +54,6 @@ namespace CMP1903M_A01_2223
             for(int i = 0; i < amountOfDeals; i++) // For loop that deals cards until the 'i' int reaches the value inputted by user
             {
                 Console.WriteLine("Press Enter For Next Card...");
-                Console.ReadLine();
                 Card dealCard = Pack.deal();
                 Console.WriteLine(dealCard.Value + " of " + dealCard.Suit); // Returns the card in a format the user can understand
                 Console.ReadLine();
