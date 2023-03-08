c78ac934351d5ea66969673074687bd7c6ad3087
Marcus Sanghvi <26944375@students.lincoln.ac.uk>
2023-03-08 20:47:11 +0000

    final

--- CMP1903M A01 2223/Testing.cs
+++ CMP1903M A01 2223/Testing.cs
@@ -20,28 +20,38 @@ namespace CMP1903M_A01_2223
 
             if(userShuffle == "y")
             {
-                Console.WriteLine("Input 1 - Fisher Yates Shuffle, 2 - Riffle Shuffle");
-                int shuffleType = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input 1 - Fisher Yates Shuffle, 2 - Riffle Shuffle, 3 - No Shuffle");
 
-                if(shuffleType >= 1 && shuffleType <= 2)
+                try
                 {
-                    Console.WriteLine("Pack Shuffled Successfully... Dealing a Card");
+                    int shuffleType = Convert.ToInt32(Console.ReadLine());
 
-                    Pack.shuffleCardPack(shuffleType);
-                    Card dealedCard = Pack.deal();
+                    if (shuffleType >= 1 && shuffleType <= 2)
+                    {
+                        Console.WriteLine("Pack Shuffled Successfully... Dealing a Card");
 
-                    Console.WriteLine(dealedCard.Value + " of " + dealedCard.Suit); // Returns the card in a format the user can understand
-                    
+                        Pack.shuffleCardPack(shuffleType);
+                        Card dealedCard = Pack.deal();
+
+                        Console.WriteLine(dealedCard.Value + " of " + dealedCard.Suit); // Returns the card in a format the user can understand
+
+                    }
+                    if(shuffleType == 3)
+                    {
+                        Console.WriteLine("No Shuffle.. Dealing a Card");
+
+                        Pack.shuffleCardPack(3);
+                        Card dealedCard = Pack.deal();
+
+                        Console.WriteLine(dealedCard.Value + " of " + dealedCard.Suit); // Returns the card in a format the user can understand
+                    }
                 }
-                else
+                catch
                 {
-                    Console.WriteLine("No Shuffle.. Dealing a Card");
-
-                    Pack.shuffleCardPack(3);
-                    Card dealedCard = Pack.deal();
-
-                    Console.WriteLine(dealedCard.Value + " of " + dealedCard.Suit); // Returns the card in a format the user can understand
+                    Console.WriteLine("Invalid Input... Restarting");
+                    Main(args);
                 }
+                
             }
             else
             {
@@ -49,15 +59,26 @@ namespace CMP1903M_A01_2223
             }
 
             Console.WriteLine("How many more cards would you like to deal?"); // Allows users to deal a specified amount of cards
-            int amountOfDeals = Convert.ToInt32(Console.ReadLine());
             
-            for(int i = 0; i < amountOfDeals; i++) // For loop that deals cards until the 'i' int reaches the value inputted by user
+
+            try
             {
-                Console.WriteLine("Press Enter For Next Card...");
-                Card dealCard = Pack.deal();
-                Console.WriteLine(dealCard.Value + " of " + dealCard.Suit); // Returns the card in a format the user can understand
-                Console.ReadLine();
+                int amountOfDeals = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < amountOfDeals; i++) // For loop that deals cards until the 'i' int reaches the value inputted by user
+                {
+                    Console.WriteLine("Press Enter For Next Card...");
+                    Card dealCard = Pack.deal();
+                    Console.WriteLine(dealCard.Value + " of " + dealCard.Suit); // Returns the card in a format the user can understand
+                    Console.ReadLine();
+                }
             }
+            catch
+            {
+                Console.WriteLine("Invalid Input... Restarting");
+                Main(args);
+            }
+            
         }
     }
 }
